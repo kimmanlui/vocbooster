d8d2c8104e3131edee9478fedd415acd42b407c3
kimmanlui <cskmlui@hotmail.com>
2019-12-12 03:14:19 +0800

    remove files

--- mysql/add_student.sql
+++
@@ -1,7 +0,0 @@
-
-
-
-select concat('insert into users_data (username, password, name) values (\'',student_mobile, '\', \'', DATE_FORMAT(birthday, ' %Y%m%d') , '\', \'',  English_Name ,'\');')   as s from student_master_all where ID=4832
-
-select concat('insert into users_data (username, password, name) values (\'',student_mobile, '\', \'', DATE_FORMAT(birthday, '%Y%m%d') , '\', \'',  English_Name ,'\');')   as s 
-from student_master_all where  vocbooster='Y'
